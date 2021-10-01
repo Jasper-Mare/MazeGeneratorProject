53d503086842e947ecc5f79427a839a538e02e50
Jasper Mare <15MareJas@poolegrammar.com>
2021-10-01 11:37:47 +0100

    Changes 01/10/2021

--- MazeGeneratorProject/User.cs
+++ MazeGeneratorProject/User.cs
@@ -28,17 +28,22 @@ namespace MazeGeneratorProject {
             }
         }
 
-        public static bool ReadUserFromFile(string name, out User user) {
-            user = new User();
+        private static string[] readFile(string address) { 
             string[] file = new string[0];
             try {
-                file = File.ReadAllLines(AppDomain.CurrentDomain.BaseDirectory + UserDBFileLoc); //https://stackoverflow.com/a/6041505
+                file = File.ReadAllLines(address);
             } catch (Exception ex) {
                 if (ex.GetType() == typeof(FileNotFoundException)) {
                     System.Windows.Forms.MessageBox.Show("File Not Found");
                 }
-                return false;
             }
+            return file;
+        }
+
+        public static bool ReadUserFromFile(string name, out User user) {
+            user = new User();
+            string[] file = readFile(AppDomain.CurrentDomain.BaseDirectory + UserDBFileLoc); //https://stackoverflow.com/a/6041505
+            if (file.Length == 0) { return false; }
 
             bool foundUser = false;
             foreach (string str in file) {
@@ -47,16 +52,32 @@ namespace MazeGeneratorProject {
                     foundUser = true;
                     user.Id = int.Parse(line[0]);
                     user.Name = line[1];
+
+
                     break;
                 }
             }
             return foundUser;
         }
 
-        public static User ReadUserFromFile(int id) {
+        public static bool ReadUserFromFile(int id, out User user) {
+            user = new User();
+            string[] file = readFile(AppDomain.CurrentDomain.BaseDirectory + UserDBFileLoc); //https://stackoverflow.com/a/6041505
+            if (file.Length == 0) { return false; }
+
+            bool foundUser = false;
+            foreach (string str in file) {
+                string[] line = str.Split(',');
+                if (line[0] == id.ToString()) {
+                    foundUser = true;
+                    user.Id = int.Parse(line[0]);
+                    user.Name = line[1];
 
 
-            return new User("name");
+                    break;
+                }
+            }
+            return foundUser;
         }
 
         public void SaveToFile() { 
@@ -66,6 +87,5 @@ namespace MazeGeneratorProject {
         //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
         //https://stackoverflow.com/a/6041505
     }
-
 
 }
